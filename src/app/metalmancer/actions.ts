8f0dda17d165b32a4cac4ce3e01c4a05cbe18e2d
Tiago Silva <tiago.toya@hotmail.com>
2025-03-06 20:59:46 -0300

    Fix prod url

--- src/app/metalmancer/actions.ts
+++ src/app/metalmancer/actions.ts
@@ -4,21 +4,21 @@ export async function generateBandNameByGenreId(genreId: string) {
     const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : process.env.NEXT_PUBLIC_BASE_URL || ''
     const url = `${baseUrl}/api/bands/genres/${genreId}/generate`
 
-    console.log('Request URL:', url) // Debug: Verifique a URL final da requisição
+    console.log('Request URL:', url)
 
     try {
         const response = await fetch(url, { method: 'POST' })
         if (!response.ok) {
             const errorText = await response.text()
-            console.error('Error Response:', errorText) // Debug: Exibir erro caso a resposta falhe
+            console.error('Error Response:', errorText)
             throw new Error(`HTTP error! Status: ${response.status}`)
         }
 
         const { bandName } = await response.json()
-        console.log('Generated Band Name:', bandName) // Debug: Exibir o nome da banda gerado
+        console.log('Generated Band Name:', bandName)
         return bandName
     } catch (error) {
-        console.error('Fetch Error:', error) // Debug: Exibir qualquer erro ocorrido
-        return null // Retorne um valor padrão ou nulo caso haja erro
+        console.error('Fetch Error:', error)
+        return null
     }
 }
